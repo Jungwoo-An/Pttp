a1a8cbff84de9f6d1a411b4b77b2fffcbf636b33
Jungwoo An <o1o9814@naver.com>
2018-02-22 00:02:33 +0900

    HttpSessionPool 싱글톤 클래스 추가

---
+++ Pttp/Session/HttpSessionPool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pttp.Session
+{
+    /// <summary>
+    /// 연결된 세션들을 관리하는 클래스입니다.
+    /// </summary>
+    public class HttpSessionPool
+    {
+        #region Variable
+        private static HttpSessionPool _instance = null;
+        private object _lockObj = null;
+        #endregion
+
+        #region Property
+        public HttpSessionPool Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lockObj)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new HttpSessionPool();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+        #endregion
+
+        private HttpSessionPool()
+        {
+            _lockObj = new object();
+        }
+    }
+}
