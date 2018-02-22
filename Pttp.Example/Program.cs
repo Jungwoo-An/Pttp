aa6d7eee2b85946a54641b7a3774dc3e7be654bf
Jungwoo An <o1o9814@naver.com>
2018-02-23 00:07:39 +0900

    예제 프로젝트 추가

---
+++ Pttp.Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace Pttp.Example
+{
+    class Program
+    {
+        static void Log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void Main(string[] args)
+        {
+            using (var server = new HttpServer(IPAddress.Any, 8085))
+            {
+                server
+                    .Logger(Log)
+                    .Start();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
