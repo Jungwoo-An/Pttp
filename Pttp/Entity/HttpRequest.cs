8dfc61d51bfb435da66798930b5585baef51f3d9
Jungwoo An <o1o9814@naver.com>
2018-02-24 14:58:00 +0900

    사용자 요청에 대응하는 HttpRequest 클래스 추가

---
+++ Pttp/Entity/HttpRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Pttp.Entity
+{
+    public class HttpRequest
+    {
+        public HttpMethod Method { get; set; }
+        public string RequestUrl { get; set; }
+        public string RequestHttpVersion { get; set; }
+    }
+}
