1f6a1bedf0aca543b5e9805fffd63b0c4d3d4246
Jungwoo An <o1o9814@naver.com>
2018-02-24 15:05:31 +0900

    지원하지 않는 Http 요청에 대응하는 NotSupportedMethodException 추가

---
+++ Pttp/Exception/NotSupportedMethodException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pttp.Exception
+{
+    public class NotSupportedMethodException : System.Exception
+    {
+        public NotSupportedMethodException(string method) : base($"Not supported http method: {method}") { }
+    }
+}
