cee1aa55f815011e57ad163f93c8da42f97176f5
Jungwoo-An <o1o9814@naver.com>
2018-03-06 23:51:56 +0900

    HttpRoutePool 라우팅 메소드 관리 클래스 추가

---
+++ Pttp/Route/HttpRoutePool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Pttp.Route
+{
+    public class HttpRoutePool
+    {
+        #region Variable
+        private static HttpRoutePool _instance = null;
+        private static object _lockObj = new object();
+        private Dictionary<string, MethodInfo> _routes = new Dictionary<string, MethodInfo>();
+        #endregion
+
+        #region Property
+        public static HttpRoutePool Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lockObj)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new HttpRoutePool();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+        #endregion
+
+        private HttpRoutePool()
+        {
+        }
+    }
+}
