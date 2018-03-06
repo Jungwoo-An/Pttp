49496ad8bad2df499bf64a70507cdb1a39d0d7b0
Jungwoo-An <o1o9814@naver.com>
2018-03-07 00:30:36 +0900

    HttpRoute 라우팅 정보를 갖는 어트리뷰트 클래스 추가

---
+++ Pttp/Route/HttpRoute.cs
@@ -0,0 +1,25 @@
+﻿using Pttp.Enums;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+
+namespace Pttp.Route
+{
+    /// <summary>
+    /// Request 정보를 갖는 어트리뷰트로 사용 되는 클래스
+    /// </summary>
+    public class HttpRoute : Attribute
+    {
+        public string Path { get; set; }
+        public Method Method { get; set; }
+        internal MethodInfo Invoker { get; set; }
+
+        public HttpRoute(string path, Method httpMethod)
+        {
+            this.Path = path;
+            this.Method = httpMethod;
+        }
+    }
+}
